73a59f451aec53d468775d79b7f9f9ba0fa28442
marcos-messias <131801840+marcos-messias@users.noreply.github.com>
2024-11-12 12:54:32 -0300

    Add files via upload

--- perfil.js
+++ perfil.js
@@ -1,30 +1,25 @@
-document.getElementById('fetch-profile').addEventListener('click', () => {
-    const url = 'http://10.111.9.76:3000/profile';
 
-    // Fazendo uma requisição GET
-    fetch(url)
-        .then(response => {
-            // Verifica se a resposta foi bem-sucedida
-            if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
-            }
-            // Converte a resposta para JSON
-            return response.json();
-        })
-        .then(data => {
-            // Preenche os campos do formulário com os dados recebidos
-            document.getElementById('nome').value = data.name || '';
-            document.getElementById('cpf').value = data.cpf || '';
-            document.getElementById('nascimento').value = data.nascimento || '';
-            document.getElementById('email').value = data.email || '';
-            document.getElementById('rg').value = data.rg || '';
-            document.getElementById('genero').value = data.genero || '';
-            document.getElementById('logradouro').value = data.logradouro || '';
-            document.getElementById('senha').value = data.senha || '';
-            document.getElementById('perfil').checked = data.perfil || false; // Marca ou desmarca o checkbox
-        })
-        .catch(error => {
-            // Captura e trata erros
-            console.error('Houve um problema com a requisição:', error);
-        });
-});
+fetch('http://10.111.9.76:3000/perfil/2')
+    .then(response => response.json())
+    .then(usuarios => {
+        if (usuarios.length > 0) {
+            const id = usuarios[0].id; 
+            preencherFormulario(usuarios[0])
+        } else {
+            throw new Error("Nenhum usuário encontrado.");
+        }
+    })
+    .catch(error => console.error('Erro ao buscar dados:', error));
+
+// Função para preencher o formulário com os dados do usuário
+function preencherFormulario(data) {
+    document.getElementById('nome').value = data.nome || '';
+    document.getElementById('cpf').value = data.cpf || '';
+    document.getElementById('nascimento').value = data.dt_nascimento || '';
+    document.getElementById('email').value = data.email || '';
+    document.getElementById('rg').value = data.rg || '';
+    document.getElementById('genero').value = data.genero || '';
+    document.getElementById('logradouro').value = data.endereco || '';
+    document.getElementById('senha').value = data.senha || '';
+    document.getElementById('perfil').checked = data.perfil || false; // Marca ou desmarca o checkbox
+}
